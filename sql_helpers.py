6a38afdfc7daaca15bb62f7090f801e829d34f00
eszemp <146843849+eszemp@users.noreply.github.com>
2023-10-05 15:56:18 +0200

    Update sql_helpers.py

---
+++ sql_helpers.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2.extensions import AsIs
+import pandas as pd
+
+def read_sql(sql, dbname, port, user, password, host ):
+    """execute sql query on database and return results"""
+    """aufrufen mit data = read_sql(sql_string, **db_credentials)"""
+
+    conn = psycopg2.connect(dbname=dbname, port=port, user=user, password=password, host=host)
+    cur = conn.cursor()
+    
+    cur.execute(sql)
+    conn.commit()
+
+    #get results and clean up
+    results = cur.fetchall()
+    conn.close()
+    return results
+
+def write_sql(sql, dbname, port, user, password, host ):
+    ###
+    return 0
+
+db_credentials = {"dbname": 'gta',
+                  "port": 5432,
+                  "user": 'gta_p8',
+                  "password": 'r7sdkfdq',
+                  "host": 'ikgpgis.ethz.ch'}
