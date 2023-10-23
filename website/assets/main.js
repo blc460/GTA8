23a663d9cbb878963c0815ed3cb0d1d860fdfc9b
blc460 <46487757+blc460@users.noreply.github.com>
2023-10-23 10:56:18 +0200

    Website erstellt

---
+++ website/assets/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	console.log("ready!");
+
+	// Map.
+
+	var map = L.map('map').fitWorld();
+
+	L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
+		minZoom: 2,
+		maxZoom: 20,
+		attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		ext: 'png'
+	}).addTo(map);
+
+	map.locate({ setView: true, maxZoom: 16 });
+
+	function onLocationFound(e) {
+		var radius = e.accuracy;
+
+		L.circle(e.latlng, radius).addTo(map);
+	}
+
+	map.on('locationfound', onLocationFound);
+
+	function onLocationError(e) {
+		alert(e.message);
+	}
+
+	map.on('locationerror', onLocationError);
+
+
+});
+
+
