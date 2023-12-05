b24a7216763a2d834c70ea67e4271d8e9dc0e27d
blc460 <christiam.blaser@gmail.com>
2023-12-05 11:25:05 +0100

    .

--- website/assets/commutes.js
+++ website/assets/commutes.js
@@ -1,18 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Sample trip data
-    var trips = [
-        { name: 'Trip 1', location: 'City A' },
-        { name: 'Trip 2', location: 'City B' },
-        // Add more trips as needed
-    ];
+    // Function to fetch trips from GeoServer
+    function fetchTrips() {
+        $.ajax({
+            type: "GET",
+            url: "YOUR_GEOSERVER_URL", // Replace with your GeoServer URL
+            dataType: "json",
+            success: function (data) {
+                // Extract the trips from the GeoServer response
+                var trips = extractTripsFromGeoServerResponse(data);
 
-    var tripList = document.getElementById('trip-list');
+                // Populate the trip list
+                var tripList = document.getElementById('trip-list');
+                trips.forEach(function (trip) {
+                    var tripItem = document.createElement('li');
+                    tripItem.className = 'trip-item';
+                    tripItem.textContent = trip.name + ' - ' + trip.location;
+                    tripList.appendChild(tripItem);
+                });
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log("Error fetching trips from GeoServer");
+                console.log(xhr.status);
+                console.log(thrownError);
+            }
+        });
+    }
 
-    // Populate the trip list
-    trips.forEach(function (trip) {
-        var tripItem = document.createElement('li');
-        tripItem.className = 'trip-item';
-        tripItem.textContent = trip.name + ' - ' + trip.location;
-        tripList.appendChild(tripItem);
-    });
+    // Function to extract trips from GeoServer response
+    function extractTripsFromGeoServerResponse(geoServerResponse) {
+        // Implement the logic to extract trips from the GeoServer response
+        // This will depend on the structure of your GeoServer response
+        // Modify this function based on the actual structure of your data
+        var trips = []; // Placeholder, replace with actual logic
+        return trips;
+    }
+
+    // Fetch trips when the page loads
+    fetchTrips();
 });
